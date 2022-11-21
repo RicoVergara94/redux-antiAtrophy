e528dae8466ec0c1472a28b8f8766985690c9a31
Oscar Vergara <oscar.vergara1994@gmail.com>
2022-11-21 05:30:21 -0500

    added funcitonality to counter

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,20 @@
 import { createStore } from "redux";
 
-const reducerFn = (state = { count: 0 }, action) => {};
+const reducerFn = (state = { counter: 0 }, action) => {
+  // synchronous
+  // we should not mutate the original state
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + action.payload,
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - action.payload,
+    };
+  }
+  return state;
+};
 
-const store = createStore(reducer);
+const store = createStore(reducerFn);
 export default store;
